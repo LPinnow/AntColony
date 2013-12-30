c46094eea8c92eb4c73b5aa0ee2cd5ff990e0d9d
L Pinnow <lpinnow3@gmail.com>
2013-12-30 13:27:40 -0600

    Driver for Simulation

---
+++ AntColonyDriver.java
@@ -0,0 +1,16 @@
+
+public class AntColonyDriver 
+{
+	/**
+     * Main method
+     * Creates a new Ant Colony
+	 * @param args
+	 */
+      public static void main(String args[])
+      {
+    	  new AntColony();
+      }
+  
+	
+	
+}
